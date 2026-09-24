a6a3aebb66d24b69669cb4b14cceb39e85b82b36
Arthur <arthur@192.168.1.75>
2026-09-24 22:28:23 +0800

    Release v2.3.9-testpre.7 for in-app update testing

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.v2ray.ang"
         minSdk = 24
         targetSdk = 37
-        versionCode = 756
-        versionName = "2.3.9-testpre.6"
+        versionCode = 757
+        versionName = "2.3.9-testpre.7"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
         splits {
